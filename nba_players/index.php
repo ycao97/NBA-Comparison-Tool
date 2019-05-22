eac01084b30f966bba804ff716a0095dd2c387c8
Yangyang Cao <ycao97@gmail.com>
2019-05-21 21:58:58 -0400

    Add files via upload

--- nba_players/index.php
+++ nba_players/index.php
@@ -6,26 +6,19 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title> NBA Player Comparison </title>
+        <title> NBA Stat Tracker </title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="jquery-ui.css" />
         <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
         <link rel="stylesheet" href="style.css" />
-        <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700" rel="stylesheet">
 
     </head>
     <body>
-        <nav class="navbar navbar-dark bg-dark">
-            <a href="index.php" class="navbar-brand mb-0 h1">
-                <img src="img/logo.png" width='60' height='50' class="d-inline-block align-top logo">
-                <h1 style="display:inline; color:rgb(130,130,130)">|</h1>
-                <h1 style="display:inline">NBA Player Comparison</h1>
-            </a>
-        </nav>
-        <div class="container-fluid">
+       <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
+                <h1>Player Comparison Finder</h1>
 
                 <form action="" name="searchForm" id="searchForm" method="POST">
                 <div class="col-lg-8">
@@ -40,7 +33,33 @@
                         <tr>
                             <td>
                             <div class="form-group">
-                                <input type="text" name="player[]" class="form-control playerTxt" placeholder="Player 1" />
+                                <input type="text" name="player[]" class="form-control playerTxt" placeholder="player1" />
+                                <input type="hidden" name="playerId[]" class="form-control playerId" value="0" />
+                             </div>
+                            </td>
+
+                            <td>
+                            <div class="form-group">
+                               <select name="year[]" class="form-control year" >
+                                <option value="2008">2008</option>
+                                <option value="2009">2009</option>
+                                <option value="2010">2010</option>
+                                <option value="2011">2011</option>
+                                <option value="2012">2012</option>
+                                <option value="2013">2013</option>
+                                <option value="2014">2014</option>
+                                <option value="2015">2015</option>
+                                <option value="2016">2016</option>
+                                <option value="2017">2017</option>
+                                </select>
+                             </div>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                            <div class="form-group">
+                                <input type="text" name="player[]" class="form-control playerTxt" placeholder="player2" />
                                 <input type="hidden" name="playerId[]" class="form-control playerId" value="0" />
                              </div>
                             </td>
@@ -66,7 +85,7 @@
                         <tr>
                             <td>
                             <div class="form-group">
-                                <input type="text" name="player[]" class="form-control playerTxt" placeholder="Player 2" />
+                                <input type="text" name="player[]" class="form-control playerTxt" placeholder="player3" />
                                 <input type="hidden" name="playerId[]" class="form-control playerId" value="0" />
                              </div>
                             </td>
@@ -92,7 +111,7 @@
                         <tr>
                             <td>
                             <div class="form-group">
-                                <input type="text" name="player[]" class="form-control playerTxt" placeholder="Player 3" />
+                                <input type="text" name="player[]" class="form-control playerTxt" placeholder="player4" />
                                 <input type="hidden" name="playerId[]" class="form-control playerId" value="0" />
                              </div>
                             </td>
@@ -118,7 +137,7 @@
                         <tr>
                             <td>
                             <div class="form-group">
-                                <input type="text" name="player[]" class="form-control playerTxt" placeholder="Player 4" />
+                                <input type="text" name="player[]" class="form-control playerTxt" placeholder="player5" />
                                 <input type="hidden" name="playerId[]" class="form-control playerId" value="0" />
                              </div>
                             </td>
@@ -144,33 +163,7 @@
                         <tr>
                             <td>
                             <div class="form-group">
-                                <input type="text" name="player[]" class="form-control playerTxt" placeholder="Player 5" />
-                                <input type="hidden" name="playerId[]" class="form-control playerId" value="0" />
-                             </div>
-                            </td>
-
-                            <td>
-                            <div class="form-group">
-                               <select name="year[]" class="form-control" >
-                                <option value="2008">2008</option>
-                                <option value="2009">2009</option>
-                                <option value="2010">2010</option>
-                                <option value="2011">2011</option>
-                                <option value="2012">2012</option>
-                                <option value="2013">2013</option>
-                                <option value="2014">2014</option>
-                                <option value="2015">2015</option>
-                                <option value="2016">2016</option>
-                                <option value="2017">2017</option>
-                                </select>
-                             </div>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                            <div class="form-group">
-                                <input type="text" name="player[]" class="form-control playerTxt" placeholder="Player 6" />
+                                <input type="text" name="player[]" class="form-control playerTxt" placeholder="player6" />
                                 <input type="hidden" name="playerId[]" class="form-control playerId" value="0" />
                              </div>
                             </td>
@@ -198,7 +191,7 @@
 
                 <div class="row">
             <div class="col-lg-8 text-center">
-                <button  type="button" class="btn btn-success btn-lg" name="result" id="resultButton" value="result">Compare</button>
+                <button  type="button" class="btn btn-primary btn-lg" name="result" id="resultButton" value="result">Get Result</button>
                 </div>
                 </div>
                 </form>
@@ -234,27 +227,37 @@
 
                     // Fetch data
                     $.ajax({
-                    url: "fetch.php",
-                    type: 'post',
-                    dataType: "json",
-                    data: {
-                    search: request.term
-                    },
-                    success: function( data ) {
-                        console.log(data);
-                        response(data);
-                    }
-                    });
-                    },
-                    select: function (event, ui) {
-                    // Set selection
-                    var txt = ui.item.label + "("+ui.item.value+")";
-                    $(this).val(txt); // display the selected text
+                        url: "fetch.php",
+                        type: 'post',
+                        dataType: "json",
+                        data: {
+                        search: request.term
+                        },
+                        success: function( data ) {                        
+                            response( data );
+                        }
+                        });
+                        },
+                        select: function (event, ui) {
+                            // Set selection
+                            var txt = ui.item.label + "("+ui.item.value+")";
+                            $(this).val(txt); // display the selected text
 
-                    $(this).next(".playerId").val(ui.item.label);
-                    return false;
-                    }
-            });
+                            $(this).next(".playerId").val(ui.item.label);
+                            updateYear(ui.item.label,ui.item.value,this);
+                            return false;
+                        }
+                    
+                 });
+
+                $('.playerTxt').each(function() {
+                    $(this).data('ui-autocomplete')._renderItem = function (ul, item) {
+                        return $( "<li>" )
+                        .append( "<a>" + item.label + "(" + item.value + ")</a>" )
+                        .appendTo( ul );
+                    };
+                });
+
 
 
             $("#resultButton").on('click', function(){
@@ -264,36 +267,52 @@
                 var validcount = 0;
 
                 for(i=0; i< playerTxt.length; i++){
-
                     if($(playerTxt[i]).val() != "" && $(playerId[i]).val() != 0){
                         validcount++;
                     }else{
                         //
                     }
-
                 }
 
                 if(validcount >= 2){
-
-
                        // Fetch data
                     $.ajax({
                         url: "resultData.php",
                         type: 'post',
                         data:  $('#searchForm').serialize()+"&action=getResult",
                     success: function( data ) {
-                        console.log(data);
+                     // console.log(data);
                         $("#result_container").html(data);
                     }
                     });
-
-
                 }else{
                     alert("Select atleast 2 players..");
                 }
-
             });
-
         });
+
+        function updateYear(player,team,ele){
+            var selElement = $(ele).parent('div').parent('td').parent('tr').find("select");
+
+            $.ajax({
+                        url: "getPlayerYears.php",
+                        type: 'post',
+                        dataType: "json",
+                        data: {
+                            player: player,
+                            team: team
+                        },
+                        success: function( data ) { 
+                            $(selElement).empty();                       
+                            $.each(data, function(index, element) {
+                                $(selElement)
+                                        .append($("<option></option>")
+                                        .attr("value",element.year)
+                                        .text(element.year)); 
+                            });
+                        }
+                });
+
+        }
     </script>
 </html>
